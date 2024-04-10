464f88d1446cb473d3396e3f339f53ee0f67e001
koon234 <hninayethu779@gmail.com>
2024-04-10 17:29:49 +0630

    Genre model and migrations

---
+++ app/Models/Genre.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Genre extends Model
+{
+    use HasFactory;
+    protected $primaryKey = 'genre_id'; 
+    
+    protected $fillable = ['genre_name']; 
+}
+
+
