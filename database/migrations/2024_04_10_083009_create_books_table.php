178a469394a0ec2d630533e229ade0c52c64fbde
koon234 <hninayethu779@gmail.com>
2024-04-10 17:30:02 +0630

    Book model and migrations

---
+++ database/migrations/2024_04_10_083009_create_books_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+                Schema::create('books', function (Blueprint $table) {
+                $table->id('book_id');
+                $table->foreignId('category_id');
+                $table->foreignId('sub_category_id');
+                $table->string('title');
+                $table->string('author');
+                $table->text('description');
+                $table->decimal('price', 8, 2);
+                $table->unsignedTinyInteger('rating');
+                $table->string('front_cover_url');
+                $table->string('back_cover_url');
+                $table->timestamps();
+            });
+      
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
