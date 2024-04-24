b3351f21ecc7f4d361afe7c5a53a5b8640732b09
koon234 <hninayethu779@gmail.com>
2024-04-24 21:37:44 +0630

    Genres sidebar

--- resources/views/management/layouts/sidebar.blade.php
+++ resources/views/management/layouts/sidebar.blade.php
@@ -4,7 +4,7 @@
         <ul class="nav flex-column">
             <li><a href="{{ route('management.categories.index') }}">Categories</a></li>
             <li><a href="{{ route('management.subcategories.index') }}">Sub Categories</a></li>
-            <li><a href="#">Genres</a></li>
+            <li><a href="{{ route('management.genres.index') }}">Genres</a></li>
             <li><a href="{{ url('/management/books') }}">Books</a></li>
         </ul>
     </div>
